a156cd5c88590cd75a7ece3e5d24335d6ebd3f56
ct101apcs <npctri.cs22@gmail.com>
2024-07-03 16:54:17 +0700

    Adjust tab layout

--- app/src/main/java/com/example/travelookup/HomeFragment.kt
+++ app/src/main/java/com/example/travelookup/HomeFragment.kt
@@ -6,12 +6,4 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 
-class HomeFragment : Fragment(R.layout.fragment_home) {
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
-    }
-}
+class HomeFragment : Fragment(R.layout.fragment_home) {}
