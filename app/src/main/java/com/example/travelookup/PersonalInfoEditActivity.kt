e957f53ad01ca2718dd3ecd858159f65e8ce34f8
ct101apcs <npctri.cs22@gmail.com>
2024-07-04 23:40:23 +0700

    Add Personal Info Edit Activity

---
+++ app/src/main/java/com/example/travelookup/PersonalInfoEditActivity.kt
@@ -0,0 +1,13 @@
+package com.example.travelookup
+
+import android.content.SharedPreferences
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class PersonalInfoEditActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.personal_info_edit)
+    }
+}
--- app/src/main/java/com/example/travelookup/ProfileFragment.kt
+++ app/src/main/java/com/example/travelookup/ProfileFragment.kt
@@ -1,5 +1,32 @@
 package com.example.travelookup
 
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 
-class ProfileFragment : Fragment(R.layout.fragment_profile) {}
+class ProfileFragment : Fragment(R.layout.fragment_profile) {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_profile, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val personalInfoText: TextView = view.findViewById(R.id.personal_in4_text)
+
+        personalInfoText.setOnClickListener {
+            Toast.makeText(context, "Edit personal info", Toast.LENGTH_SHORT).show()
+            startActivity(Intent(getActivity(), PersonalInfoEditActivity::class.java))
+        }
+    }
+}
