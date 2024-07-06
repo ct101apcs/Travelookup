7739c772687c21d303f3e8bdffb51a217f201e4a
ct101apcs <npctri.cs22@gmail.com>
2024-07-07 01:13:15 +0700

    Change avatar finish

--- app/src/main/java/com/example/travelookup/FlightBookingActivity.kt
+++ app/src/main/java/com/example/travelookup/FlightBookingActivity.kt
@@ -1,11 +1,24 @@
 package com.example.travelookup
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.sahana.horizontalcalendar.HorizontalCalendar
+import com.sahana.horizontalcalendar.OnDateSelectListener
+
 
 class FlightBookingActivity : AppCompatActivity(){
+    private lateinit var mHorizontalCalendar: HorizontalCalendar
+    private lateinit var mDateTextView: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.transport_flight)
+
+//        mHorizontalCalendar.setOnDateSelectListener(OnDateSelectListener { dateModel ->
+//            mDateTextView.setText(
+//                if (dateModel != null) dateModel.day.toString() + " " + dateModel.dayOfWeek + " " + dateModel.month + "," + dateModel.year else ""
+//            )
+//        })
     }
 }
--- app/src/main/java/com/example/travelookup/PersonalInfoEditActivity.kt
+++ app/src/main/java/com/example/travelookup/PersonalInfoEditActivity.kt
@@ -3,20 +3,44 @@ package com.example.travelookup
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
-import android.widget.ImageButton
-import android.widget.ImageView
+import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.imageview.ShapeableImageView
 
 class PersonalInfoEditActivity : AppCompatActivity() {
+    private lateinit var imagePickerLauncher: ActivityResultLauncher<Intent>
+    private var selectedImageUri: Uri? = null
+    private lateinit var profileAvatar: ShapeableImageView
+    private lateinit var changeAvatarButton: FloatingActionButton
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.personal_info_edit)
 
-        val avatar: ImageView = findViewById(R.id.profile_avatar);
+        profileAvatar = findViewById(R.id.profile_avatar)
+        changeAvatarButton = findViewById(R.id.change_avatar_button)
+
+        imagePickerLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == RESULT_OK) {
+                val data: Intent? = result.data
+                selectedImageUri = data?.data
+                if (selectedImageUri != null) {
+                    Glide.with(this).load(selectedImageUri).apply(RequestOptions.centerCropTransform()).into(profileAvatar)
+                } else {
+                    Toast.makeText(this, "Failed to load image", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+
+        changeAvatarButton.setOnClickListener {
+            val intent = Intent(Intent.ACTION_PICK)
+            intent.type = "image/*"
+            imagePickerLauncher.launch(intent)
+        }
     }
 }
